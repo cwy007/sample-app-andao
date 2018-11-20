c2224a07d588fef4239e038e37cfb52a30e7c74d
cwy007 <chanweiyan007@gmail.com>
2018-11-20 23:51:22 +0800

    Test remember_me checkbox function

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
-class ActiveSupport::TestCase
+class ActiveSupport::TestCase # 所有的测试都继承这个类，所以测试用的辅助方法要在这里定义
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
@@ -12,5 +12,17 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me}}
   end
 end
