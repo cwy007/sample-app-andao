98ef924da9c860536c9953411069df601ca6bb05
cwy007 <chanweiyan007@gmail.com>
2018-11-20 21:46:47 +0800

    Implement User#remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -8,13 +9,22 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # 当 token 值唯一时，hacker 必须知道 user_id 和 remember_token 才能实现‘会话劫持’
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
-  def User.new_token
-    SecureRandom.urlsafe_base64
+  # update_attribute 会跳过验证，直接更新数据；因为无法获取 password 的值，此处必须跳过model验证
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
