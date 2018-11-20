b6843426d624b400bc99eef4e8498c7af85488ff
cwy007 <chanweiyan007@gmail.com>
2018-11-20 23:15:17 +0800

    Test logout on one browser & visit website again on other browser

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ApplicationRecord
   end
 
   def authenticated?(remember_token)
+    return false if remember_token.blank?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
